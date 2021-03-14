95d88f6c73e2793ee7d7f37f95919e83c038401c
Alfred Tejeda <atejeda.ccs@gmail.com>
2021-03-14 12:26:18 -0500

    update testcases and update the main login question function

--- src/questions/login.js
+++ src/questions/login.js
@@ -2,7 +2,20 @@ import { t } from 'testcafe'
 import { Login } from '../ui/login'
 
 export class LoginQuestions extends Login {
-    async validateInvalidUsernameMessage() {
-        await t.expect(this.div.alert.innerText).contains('Your username is invalid!')
+    async validateLoginAttempMessage(messageType) {
+        switch (messageType) {
+            case 'invalidUsername':
+                await t.expect(this.div.alert.innerText).contains('Your username is invalid!')
+                break
+            case 'invalidPassword':
+                await t.expect(this.div.alert.innerText).contains('Your password is invalid!')
+                break
+            case 'loginSucces':
+                await t.expect(this.div.alert.innerText).contains('You logged into a secure area!')
+                break
+            default:
+                console.warn('unsupported message type: ' + messageType)
+                break;
+        }
     }
 }
--- src/tests/test.js
+++ src/tests/test.js
@@ -7,7 +7,17 @@ const LOGIN_QUESTIONS = new LoginQuestions
 fixture`First time TestCafe`
     .page`https://the-internet.herokuapp.com/login`
 
-test('First testcase in testcafe', async () => {
+test('First testcase: Login attemp with invalid username', async () => {
     await LOGIN_TASKS.loginAttempt('Alfred', 'Password')
-    await LOGIN_QUESTIONS.validateInvalidUsernameMessage()
+    await LOGIN_QUESTIONS.validateLoginAttempMessage('invalidUsername')
+})
+
+test('Second testcase: Login attemp with invalid password', async () => {
+    await LOGIN_TASKS.loginAttempt('tomsmith', 'Password')
+    await LOGIN_QUESTIONS.validateLoginAttempMessage('invalidPassword')
+})
+
+test('Third testcase: Login attemp succes', async () => {
+    await LOGIN_TASKS.loginAttempt('tomsmith', 'SuperSecretPassword!')
+    await LOGIN_QUESTIONS.validateLoginAttempMessage('loginSucces')
 })
